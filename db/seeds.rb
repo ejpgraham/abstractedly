a44b760ff1cac6f2c5891b49d0b52eb18106d765
ejpgraham <ejpgraham@gmail.com>
2017-11-16 22:27:33 -0500

    Add basic dummy data for a journal and some abstracts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the JournalDatabase with its default values.
+# The JournalData can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'journal_data'
+# Journal attributes:
+
+# t.string :title
+# t.string :url
+# t.boolean :subscribed
+# t.date :date
+# t.integer :volume
+# t.integer :issue_number
+
+# Abstract attributes
+
+# t.text :title
+# t.text :authors
+# t.text :body
+# t.string :images
+# t.string :url
+# t.boolean :visible
+# t.references :journal, index: true, foreign_key: true
+
+journals = Journal.create([
+  {title: "Journal of Nuclear Medicine" , url: "http://jnm.snmjournals.org/" , subscribed: true, date: '%2017-%11-%1' , volume: 45 , issue_number: 1}
+])
+
+abstracts = Abstract.create([
+  {title: JournalData.titles[0], authors: JournalData.authors[0], body: JournalData.bodies[0], images: "", url: "www.abstract.com", visible: true, journal: journals.sample},
+  {title: JournalData.titles[1], authors: JournalData.authors[1], body: JournalData.bodies[1], images: "", url: "www.abstract.com", visible: true, journal: journals.sample},
+  {title: JournalData.titles[2], authors: JournalData.authors[2], body: JournalData.bodies[2], images: "", url: "www.abstract.com", visible: true, journal: journals.sample},
+  {title: JournalData.titles[3], authors: JournalData.authors[3], body: JournalData.bodies[3], images: "", url: "www.abstract.com", visible: true, journal: journals.sample},
+  {title: JournalData.titles[4], authors: JournalData.authors[4], body: JournalData.bodies[4], images: "", url: "www.abstract.com", visible: true, journal: journals.sample},
+])
+
+puts "Seed finished."
+puts "#{Journal.count} journals created"
+puts "#{Abstract.count} abstracts created"
---
+++ lib/journal_data.rb
@@ -0,0 +1,57 @@
+module JournalData
+
+  def self.titles
+    [
+      "More α Than β for Prostate Cancer?",
+      "Clinical Use and Utility of Amyloid Imaging",
+      "Current Concepts in 68Ga-DOTATATE Imaging of Neuroendocrine Neoplasms: Interpretation, Biodistribution, Dosimetry, and Molecular Strategies",
+      "Metabolic Imaging of Infection",
+      "Nuclear Medicine Training in the United States"
+    ]
+  end
+
+  def self.bodies
+     [
+  "Currently, 3 amyloid PET tracers are approved and commercially available for clinical use. They allow for the accurate in vivo detection of amyloid plaques, one hallmark of Alzheimer disease. Here, we review the current knowledge on the clinical use and utility of amyloid imaging. Appropriate use criteria for the clinical application of amyloid imaging are established, and most currently available data point to their validity. Visual amyloid image analysis is highly standardized. Disclosure of amyloid imaging results is desired by many cognitively impaired subjects and seems to be safe once appropriate education is delivered to the disclosing clinicians. Regarding clinical utility, increasing evidence points to a change in diagnosis via amyloid imaging in about 30% of cases, to an increase in diagnostic confidence in about 60% of cases, to a change in patient management in about 60% of cases, and specifically to a change in medication in about 40% of cases. Also, amyloid imaging results seem to have a relevant impact on caregivers. Further, initial simulation studies point to a potential positive effect on patient outcome and to cost effectiveness of amyloid imaging. These features, however, will require confirmation in prospective clinical trials. More work is also required to determine the clinical utility of amyloid imaging specifically in subjects with mild cognitive impairment and in comparison with or in conjunction with other Alzheimer disease biomarkers. In summary, the clinical use of amyloid imaging is being studied, and the currently available data point to a relevant clinical utility of this imaging technique. Ongoing research will determine whether this accurate and noninvasive approach to amyloid plaque load detection will translate into a benefit to cognitively impaired subjects.",
+
+  "68Ga-DOTATATE PET/CT provides information on the location of somatostatin receptor–expressing tumors. Integrating this imaging data effectively in patient care requires the clinical history; the histopathology and biomarker information; and the grade, stage, and prior imaging results. Previous therapies and technical aspects of the study should be considered, given their ability to alter the interpretation of the images. This includes physiologic biodistribution of the radiotracer, as well as conditions that engender false-positive results. This article provides a guide to the performance and interpretation of 68Ga-DOTATATE PET/CT and describes its role in the diagnostic algorithm of neuroendocrine neoplasms and its overall utility in their management.",
+
+  "Metabolic imaging has come to occupy a prominent place in the diagnosis and management of microbial infection. Molecular probes available for infection imaging have undergone a rapid evolution starting with nonspecific agents that accumulate similarly in infection, sterile inflammation, and neoplastic tissue and then extending to more targeted probes that seek to identify specific microbial species. This focus review describes the metabolic and molecular imaging techniques currently available for clinical use in infection imaging and those that have demonstrated promising results in preclinical studies with the potential for clinical applications.",
+
+  "Radioimmunotherapy of solid tumors using antibody-targeted radionuclides has been limited by low therapeutic indices (TIs). We recently reported a novel 3-step pretargeted radioimmunotherapy (PRIT) strategy based on a glycoprotein A33 (GPA33)–targeting bispecific antibody and a small-molecule radioactive hapten, a complex of 177Lu and S-2-(4-aminobenzyl)-1,4,7,10-tetraazacyclododecane tetraacetic acid (177Lu-DOTA-Bn), that leads to high TIs for radiosensitive tissues such as blood (TI = 73) and kidney (TI = 12). We tested our hypothesis that a fractionated anti-GPA33 DOTA-PRIT regimen calibrated to deliver a radiation absorbed dose to tumor of more than 100 Gy would lead to a high probability of tumor cure while being well tolerated by nude mice bearing subcutaneous GPA33-positive SW1222 xenografts. Methods: We treated groups of nude mice bearing 7-d-old SW1222 xenografts with a fractionated 3-cycle anti-GPA33 DOTA-PRIT regimen (total administered 177Lu-DOTA-Bn activity, 167 MBq/mouse; estimated radiation absorbed dose to tumor, 110 Gy). In randomly selected mice undergoing treatment, serial SPECT/CT imaging was used to monitor treatment response and calculate radiation absorbed doses to tumor. Necropsy was done on surviving animals 100–200 d after treatment to determine frequency of cure and assess select normal tissues for treatment-related histopathologies. Results: Rapid exponential tumor progression was observed in control treatment groups (i.e., no treatment or 177Lu-DOTA-Bn only), leading to euthanasia due to excessive tumor burden, whereas 10 of 10 complete responses were observed for the DOTA-PRIT–treated animals within 30 d. Treatment was well tolerated, and 100% histologic cure was achieved in 9 of 9 assessable animals without detectable radiation damage to critical organs, including bone marrow and kidney. Radiation absorbed doses to tumor derived from SPECT/CT (102 Gy) and from biodistribution (110 Gy) agreed to within 6.9%. Of the total dose of approximately 100 Gy, the first dose contributes 30%, the second dose 60%, and the third dose 10%. Conclusion: In a GPA33-positive human colorectal cancer xenograft mouse model, we validated a SPECT/CT-based theranostic PRIT regimen that led to 100% complete responses and 100% cures without any treatment-related toxicities, based on high TIs for radiosensitive tissues. These studies support the view that anti-GPA33 DOTA-PRIT will be a potent radioimmunotherapy regimen for GPA33-positive colorectal cancer tumors in humans.",
+
+  "In recent years, inherited and acquired mutations in the tricarboxylic acid (TCA) cycle enzymes have been reported in diverse cancers. Pheochromocytomas and paragangliomas often exhibit dysregulation of glucose metabolism, which is also driven by mutations in genes encoding the TCA cycle enzymes or by activation of hypoxia signaling. Pheochromocytomas and paragangliomas associated with succinate dehydrogenase (SDH) deficiency are characterized by high 18F-FDG avidity. This association is currently only partially explained. Therefore, we hypothesized that accumulation of succinate due to the TCA cycle defect could be the major connecting hub between SDH-mutated tumors and the 18F-FDG uptake profile. Methods: To test whether succinate modifies the 18F-FDG metabolic profile of tumors, we performed in vitro and in vivo (small-animal PET/CT imaging and autoradiography) experiments in the presence of succinate, fumarate, and phosphate-buffered saline (PBS) in different cell models. As a control, we also evaluated the impact of succinate on 18F-fluorocholine uptake and retention. Glucose transporter 1 (GLUT1) immunohistochemistry was performed to assess whether 18F-FDG uptake correlates with GLUT1 staining. Results: Intratumoral injection of succinate significantly increased 18F-FDG uptake at 24 h on small-animal PET/CT imaging and autoradiography. No effect of succinate was observed on cancer cells in vitro, but interestingly, we found that succinate caused increased 18F-FDG uptake by human umbilical vein endothelial cells in a concentration-dependent manner. No significant effect was observed after intratumoral injection of fumarate or PBS. Succinate, fumarate, and PBS have no effect on cell viability, regardless of cell lineage. Intramuscular injection of succinate also significantly increases 18F-FDG uptake by muscle when compared with either PBS or fumarate, highlighting the effect of succinate on connective tissues. No difference was observed between PBS and succinate on 18F-fluorocholine uptake in the tumor and muscle and on hind limb blood flow. GLUT1 expression quantification did not significantly differ between the study groups. Conclusion: The present study shows that succinate stimulates 18F-FDG uptake by endothelial cells, a finding that partially explains the 18F-FDG metabotype observed in tumors with SDH deficiency. Although this study is an 18F-FDG–based approach, it provides an impetus to better characterize the determinants of 18F-FDG uptake in various tumors and their surrounding microenvironment, with a special emphasis on the role of tumor-specific oncometabolites."
+  ]
+  end
+
+  def self.authors
+  [
+    "Wolfgang P. Fendler and Cathy Cutler",
+    "Henryk Barthel and Osama Sabri",
+    "Lisa Bodei, Valentina Ambrosini, Ken Herrmann, and Irvin Modlin",
+    "Ismaheel Lawal, JanRijn Zeevaart, Thomas Ebenhan, Alfred Ankrah, Mariza Vorster, Hendrik G. Kruger, Thavendran Govender, and Mike Sathekge",
+    "George M. Segall, Erin E. Grady, Joanna R. Fair, Munir V. Ghesani, and Leonie Gordon"
+  ]
+
+  end
+
+  def self.keywords
+     [
+    "amyloid",
+    "PET",
+    "dementia",
+    "utility",
+    "molecular",
+    "imaging",
+    "infection",
+    "anti-microbial",
+    "optical",
+    "imaging",
+    "pretargeting",
+    "colorectal",
+    "cancer",
+    "radioimmunotherapy",
+    "SPECT"
+  ]
+  end
+end
