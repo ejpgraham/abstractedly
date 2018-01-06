5a0f7f6653d625734c142fbc3b25adff5a68c090
ejpgraham <ejpgraham@gmail.com>
2018-01-05 20:20:33 -0500

    Keywords Fix
    -Keywords index method now uses .group so @keywords contains unique keywords by :body
    -unique_keywords helper method deleted
    -keyword_bodies refactored to use .pluck

--- app/helpers/keywords_helper.rb
+++ app/helpers/keywords_helper.rb
@@ -1,14 +1,6 @@
 module KeywordsHelper
 
   def keyword_bodies(abstract)
-    abstract.keywords.map do |keyword|
-      keyword.body
-    end
+    abstract.keywords.pluck(:body)
   end
-
-  def unique_keywords(keywords)
-    unique = keywords.uniq { |keyword| keyword.body }
-    unique.sort_by { |keyword| keyword.body }
-  end
-
 end
