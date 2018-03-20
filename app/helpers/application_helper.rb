f16b6c09cfb3f5cdaa0129da4e37d26ee180f90a
ejpgraham <ejpgraham@gmail.com>
2018-03-20 16:20:08 -0400

    Add application helper back

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,24 @@
+
+module ApplicationHelper
+
+  # def format_abstract_body(abstract_body)
+  #   strong_words = ["Conclusion:", "Results:", "Methods:" ]
+  #   results = []
+  #   abstract_body.split(" ").each_with_index do |word, i|
+  #     if strong_words.include?(word)
+  #       results.push( "<br><br><strong>#{word}</strong>" )
+  #     elsif i == 0
+  #       results.push("<strong>Summary:</strong> #{word}")
+  #     else
+  #       results.push(word)
+  #     end
+  #   end
+  #   results.join(" ")
+  # end
+
+  def randomized_background_image
+  images = ["abstract1.jpg", "abstract0.jpg", "abstract2.jpg", "abstract3.jpg", "abstract4.jpg", "abstract5.jpg"]
+  images[rand(images.size)]
+  end
+
+end
